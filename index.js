f87d4ae79430b626c9978a4ac3a9946af939547e
Rediat <yaidercc@gmail.com>
2022-06-13 13:23:22 -0500

    Menu desarrollado, y clase de buscadores empezada

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
-const { leerInput, pausa, inquirerMenu } = require("./helpers/inquirer")
+// Modules and resources
 
+const { leerInput, pausa, inquirerMenu } = require("./helpers/inquirer");
+const Searches= require("./models/Searches");
+
+// Code
 
 const main =async()=>{
     let opt;
+    const searches= new Searches();
     do{
         opt=await inquirerMenu();
+
+        switch (opt) {
+            case 1:
+                // show Message
+                const place= await leerInput("Ciudad: ");
+                console.log(place);
+                // Search places
+
+                // Select places
+
+                // climate
+                console.log('\n Informacion de la ciudad. \n'.green);
+                console.log('Ciudad: ',);
+                console.log('Lat: ',);
+                console.log('Lng: ',);
+                console.log('Temperatura: ',);
+                console.log('Minima: ',);
+                console.log('Maxima: ',);
+                break;
+        
+            default:
+                break;
+        }
         if(opt!==0) await pausa();
     }while(opt!=0);
 }
---
+++ models/Searches.js
@@ -0,0 +1,13 @@
+class Searches{
+    historial=['New york','Madrid','San Jose'];
+
+    constructor(){
+        // TODO: read db if exists.
+    }
+
+    async city(place=''){
+        console.log(place);
+    }
+}
+
+module.exports=Searches;
